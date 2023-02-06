f2556b4ebb5473e947928d97560e5012886d2578
kellyngton <kelldias895@gmail.com>
2023-02-06 00:04:40 -0300

    Adicionando estlização do footer e padrão do corpo dos itens de projects.

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-// script.js
-console.log("Hello, World!");
+const projectBtns = document.querySelectorAll(".project-btn");
+
+projectBtns.forEach(function(btn) {
+  btn.addEventListener("click", function() {
+    // Adicione aqui a lógica para exibir mais informações sobre o projeto ao clicar no botão "Ver mais"
+  });
+});
+
+const navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach(function(link) {
+  link.addEventListener("click", function() {
+    // Adicione aqui a lógica para rolar para a seção correspondente ao clicar no link de navegação
+  });
+});
